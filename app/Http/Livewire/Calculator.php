4f12f82d0d766862b0f2e9871cb86c0fefa654bd
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-09 13:00:43 -0300

    Add calculator functionality to Livewire component

--- app/Http/Livewire/Calculator.php
+++ app/Http/Livewire/Calculator.php
@@ -6,20 +6,44 @@ use Livewire\Component;
 
 class Calculator extends Component
 {
-    public $number = 0;
+    public $calculation = '';
+    public $result = 0;
 
     public function render()
     {
         return view('livewire.calculator');
     }
 
-    public function increment()
-    {
-        $this->number++;
+    public function calculate(){
+        $this->result = eval('return '.$this->calculation.';');
     }
 
-    public function decrement()
+    public function addNumber($number)
     {
-        $this->number--;
+        $this->calculation .= $number;
+    }
+
+    public function addOperator($operator)
+    {
+        if($operator == '%') {
+            $this->calculation .= '/100';
+        } else {
+            $this->calculation .= $operator;
+        }
+    }
+
+    public function addDecimalPoint()
+    {
+        if($this->calculation == '') {
+            $this->calculation .= '0.';
+        } else {
+            $this->calculation .= '.';
+        }
+    }
+
+    public function clear()
+    {
+        $this->calculation = '';
+        $this->result = 0;
     }
 }
